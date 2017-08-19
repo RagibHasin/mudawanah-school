20d6a002ae22eb3c11dc472c13d9ccfc5ee2421b
Muhammad Ragib Hasin <ragib.hasin@gmail.com>
2017-08-19 18:34:57 +0600

    fix: post not working correctly

--- src/main.ts
+++ src/main.ts
@@ -9,64 +9,37 @@ import * as fs from 'fs'
 
 import { MarkdownIt } from 'markdown-it'
 
-const plugin: IPlugin = {
-  async initialize(blog: { routes: route, config: IConfig, posts: IPost[] }) {
-
-  },
-
-  postRender(post: IPost, md: MarkdownIt) {
-    if (post.pluginsData && post.pluginsData['school']) {
-      // Renderer
-      if (post.pluginsData['school'].math) {
-        md = md.use(require('markdown-it-math'), {
-          inlineOpen: '$%$',
-          inlineClose: '$%$',
-          blockOpen: '$%$$',
-          blockClose: '$$%$'
-        })
-      }
-      if (post.pluginsData['school'].chem) {
-        md = md.use(require('markdown-it-mathjax')())
-      }
-
-      // Script loading
-      if (post.pluginsData['school'].math && !post.pluginsData['school'].chem) {
-        post.pluginsData['school'].header = fs.readFileSync(__dirname + '/mathonly.html', 'utf8')
-      }
-      if (post.pluginsData['school'].math && post.pluginsData['school'].chem) {
-        post.pluginsData['school'].header = fs.readFileSync(__dirname + '/both.html', 'utf8')
-      }
+function contextRender(ctx: IPage | IPost, md: MarkdownIt) {
+  if (ctx.pluginsData && ctx.pluginsData['school']) {
+    // Renderer
+    if (ctx.pluginsData['school'].math) {
+      md = md.use(require('markdown-it-math'), {
+        inlineOpen: '%$',
+        inlineClose: '$%',
+        blockOpen: '%$$',
+        blockClose: '$$%'
+      })
+    }
+    if (ctx.pluginsData['school'].chem) {
+      md = md.use(require('markdown-it-mathjax')())
     }
 
-    return md
-  },
-
-  pageRender(page: IPage, md: MarkdownIt) {
-    if (page.pluginsData && page.pluginsData['school']) {
-      // Renderer
-      if (page.pluginsData['school'].math) {
-        md = md.use(require('markdown-it-math'), {
-          inlineOpen: '%$',
-          inlineClose: '$%',
-          blockOpen: '%$$',
-          blockClose: '$$%'
-        })
-      }
-      if (page.pluginsData['school'].chem) {
-        md = md.use(require('markdown-it-mathjax')())
-      }
-
-      // Script loading
-      if (page.pluginsData['school'].chem) {
-        page.pluginsData['school'].header = fs.readFileSync(__dirname + '/both.html', 'utf8')
-      }
-      if (page.pluginsData['school'].math) {
-        page.pluginsData['school'].header = fs.readFileSync(__dirname + '/mathonly.html', 'utf8')
-      }
+    // Script loading
+    if (ctx.pluginsData['school'].chem) {
+      ctx.pluginsData['school'].header = fs.readFileSync(__dirname + '/both.html', 'utf8')
     }
-
-    return md
+    if (ctx.pluginsData['school'].math) {
+      ctx.pluginsData['school'].header = fs.readFileSync(__dirname + '/mathonly.html', 'utf8')
+    }
   }
+
+  return md
+}
+
+const plugin: IPlugin = {
+  async initialize(blog: { routes: route, config: IConfig, posts: IPost[] }) { },
+  postRender: contextRender,
+  pageRender: contextRender
 }
 
 export = plugin
